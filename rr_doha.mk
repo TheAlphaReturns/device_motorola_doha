1af4f2e7af11641e07a6a197eff3c186cda5dd8e
TheAlphaReturns <angad.tendulkar@gmail.com>
2021-10-08 08:35:03 -0400

    Update to latest stock (QPIS30.28.26.4.1.6)

--- rr_doha.mk
+++ rr_doha.mk
@@ -60,10 +60,10 @@ PRODUCT_MODEL := moto g8 plus
 TARGET_DEVICE := Moto G8 Plus
 PRODUCT_SYSTEM_NAME := Moto G8 Plus
 
-VENDOR_RELEASE := 10/QPI30.28-Q3-28-26-4-1/2f8cb:user/release-keys
+VENDOR_RELEASE := 10/QPIS30.28-Q3-28-26-4-1-6/9976a:user/release-keys
 BUILD_FINGERPRINT := motorola/doha_retail/doha:$(VENDOR_RELEASE)
 RR_BUILD_FINGERPRINT := motorola/doha_retail/doha:$(VENDOR_RELEASE)
-RR_PRIVATE_BUILD_DESC := "'doha_retail-user 10 QPI30.28-Q3-28-26-4-1 2f8cb release-keys'"
+RR_PRIVATE_BUILD_DESC := "'doha_retail-user 10 QPIS30.28-Q3-28-26-4-1-6 9976a release-keys'"
 
 TARGET_VENDOR := motorola
 
